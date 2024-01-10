10f15b3b3c4e20158ff35f6d302c1ffd5769f88a
Nebojša Vuga <vuga.sv53.2020@uns.ac.rs>
2024-01-10 15:59:09 +0100

    added name

--- ZutaKuca/main.cpp
+++ ZutaKuca/main.cpp
@@ -19,6 +19,7 @@
 
 const unsigned int wWidth = 800;
 const unsigned int wHeight = 600;
+static unsigned loadImageToTexture(const char* filePath); //Ucitavanje teksture, izdvojeno u funkciju
 
 int main()
 {
@@ -48,6 +49,40 @@ int main()
     }
 
 
+    Shader nameShader("name.vert", "name.frag");
+    float nameVertices[] =
+    {
+        -0.98, -0.7, 0.0, 1.0,
+        -0.98, -1.0, 0.0, 0.0,
+        -0.5, -0.7, 1.0, 1.0,
+        -0.5, -1.0, 1.0, 0.0
+    };
+
+    unsigned int nameIndices[] =
+    {
+        0, 1, 3,
+        3, 2, 0
+    };
+    unsigned int VAOPERSONAL;
+    glGenVertexArrays(1, &VAOPERSONAL);
+    glBindVertexArray(VAOPERSONAL);
+
+    unsigned int VBOPERSONAL;
+    glGenBuffers(1, &VBOPERSONAL);
+    glBindBuffer(GL_ARRAY_BUFFER, VBOPERSONAL);
+
+    glBufferData(GL_ARRAY_BUFFER, sizeof(nameVertices), nameVertices, GL_STATIC_DRAW);
+    glVertexAttribPointer(0, 2, GL_FLOAT, GL_FALSE, 4 * sizeof(float), (void*)0);
+    glEnableVertexAttribArray(0);
+    glVertexAttribPointer(1, 2, GL_FLOAT, GL_FALSE, 4 * sizeof(float), (void*)(2 * sizeof(float)));
+    glEnableVertexAttribArray(1);
+
+    unsigned int EBOPERSONAL;
+    glGenBuffers(1, &EBOPERSONAL);
+    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBOPERSONAL);
+    glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(nameIndices), nameIndices, GL_STATIC_DRAW);
+
+
     Model lija("res/low-poly-fox.obj");
     //Tjemena i baferi su definisani u model klasi i naprave se pri stvaranju objekata
 
@@ -64,7 +99,21 @@ int main()
     unifiedShader.setMat4("uV", view);
     glm::mat4 model = glm::mat4(1.0f);
 
+
+    unsigned name = loadImageToTexture("res/imebelo.png");
+    glBindTexture(GL_TEXTURE_2D, name);
+    glGenerateMipmap(GL_TEXTURE_2D);
+    //glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
+    //glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST);
+    glBindTexture(GL_TEXTURE_2D, 0);
+
+    // omogucavanje dubine
     glEnable(GL_DEPTH_TEST);
+
+    glCullFace(GL_BACK);
+
     while (!glfwWindowShouldClose(window))
     {
 
@@ -72,9 +121,22 @@ int main()
             glfwSetWindowShouldClose(window, true);
 
         glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+
         model = glm::rotate(model, glm::radians(0.1f), glm::vec3(0.0f, 1.0f, 0.0f));
         unifiedShader.setMat4("uM", model);
         lija.Draw(unifiedShader);
+
+
+        glEnable(GL_BLEND);
+        glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
+        nameShader.use();
+        glBindVertexArray(VAOPERSONAL);
+        glActiveTexture(GL_TEXTURE0);
+        glBindTexture(GL_TEXTURE_2D, name);
+        glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, (void*)(0 * sizeof(unsigned int)));
+        glDisable(GL_BLEND);
+        glUseProgram(0);
+        glBindVertexArray(0);
 
         glfwSwapBuffers(window);
         glfwPollEvents();
@@ -82,6 +144,44 @@ int main()
 
     glfwTerminate();
     return 0;
+}
+
+
+static unsigned loadImageToTexture(const char* filePath) {
+    int TextureWidth;
+    int TextureHeight;
+    int TextureChannels;
+    unsigned char* ImageData = stbi_load(filePath, &TextureWidth, &TextureHeight, &TextureChannels, 0);
+    if (ImageData != NULL)
+    {
+        //Slike se osnovno ucitavaju naopako pa se moraju ispraviti da budu uspravne
+        stbi__vertical_flip(ImageData, TextureWidth, TextureHeight, TextureChannels);
+
+        // Provjerava koji je format boja ucitane slike
+        GLint InternalFormat = -1;
+        switch (TextureChannels) {
+        case 1: InternalFormat = GL_RED; break;
+        case 3: InternalFormat = GL_RGB; break;
+        case 4: InternalFormat = GL_RGBA; break;
+        default: InternalFormat = GL_RGB; break;
+        }
+
+        unsigned int Texture;
+        glGenTextures(1, &Texture);
+        glBindTexture(GL_TEXTURE_2D, Texture);
+        glTexImage2D(GL_TEXTURE_2D, 0, InternalFormat, TextureWidth, TextureHeight, 0, InternalFormat, GL_UNSIGNED_BYTE, ImageData);
+        glBindTexture(GL_TEXTURE_2D, 0);
+
+        // oslobadjanje memorije zauzete sa stbi_load posto vise nije potrebna
+        stbi_image_free(ImageData);
+        return Texture;
+    }
+    else
+    {
+        std::cout << "Textura nije ucitana! Putanja texture: " << filePath << std::endl;
+        stbi_image_free(ImageData);
+        return 0;
+    }
 }
 
 
